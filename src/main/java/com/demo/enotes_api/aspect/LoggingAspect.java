2aafe5f51deda76f96f315fb2a8617f5eb05e765
vinodsajgure <vin.sajgure@gmail.com>
2025-05-20 06:49:20 +0530

    AOP implemented.

---
+++ src/main/java/com/demo/enotes_api/aspect/LoggingAspect.java
@@ -0,0 +1,65 @@
+package com.demo.enotes_api.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+//	@Before("execution('com.demo.enotes_api.controller..'(..))")
+//	public void beforeController(JoinPoint joinPoint) {
+//		
+//		Signature signature = joinPoint.getSignature();
+//		String className = signature.getDeclaringType().getSimpleName();
+//		String methodName = signature.getName();
+//		log.info("Calling :: {} :: {} ()",className,methodName);
+//	}
+//	
+//	@After("execution('com.demo.enotes_api.controller..'(..))")
+//	public void AfterController(JoinPoint joinPoint) {
+//		
+//		Signature signature = joinPoint.getSignature();
+//		String className = signature.getDeclaringType().getSimpleName();
+//		String methodName = signature.getName();
+//		log.info("End Calling :: {} :: {} ()",className,methodName);
+//	}
+	
+	@Around("execution('com.demo.enotes_api.controller..'(..))")
+	public Object JoinPointController(ProceedingJoinPoint joinPoint) throws Throwable {
+		
+		Signature signature = joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+		log.info("Calling :: {} :: {} ()",className,methodName);
+		long start = System.currentTimeMillis();
+		Object result = joinPoint.proceed();
+		long duration = System.currentTimeMillis()-start;
+		log.info("End Calling :: {} :: {} :: {} ms",className,methodName, duration);
+		return result;
+	}
+	
+	@Around("execution('com.demo.enotes_api.service..'(..))")
+	public Object JoinPointService(ProceedingJoinPoint joinPoint) throws Throwable {
+		
+		Signature signature = joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+		log.info("Calling :: {} :: {} ()",className,methodName);
+		long start = System.currentTimeMillis();
+		Object result = joinPoint.proceed();
+		long duration = System.currentTimeMillis()-start;
+		log.info("End Calling :: {} :: {} :: {} ms",className,methodName, duration);
+		return result;
+	}
+	
+}
